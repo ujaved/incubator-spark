ca77b4d830be73240478f90c16f837325081ca43
Italo Cunha <cunha@dcc.ufmg.br>
2013-11-19 01:14:14 -0800

    scala scripts

---
+++ scripts/SimpleApp.scala
@@ -0,0 +1,15 @@
+/*** SimpleApp.scala ***/
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "$YOUR_SPARK_HOME/README.md" // Should be some file on your system
+    val sc = new SparkContext("local", "Simple App", "YOUR_SPARK_HOME",
+      List("target/scala-2.9.3/simple-project_2.9.3-1.0.jar"))
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+  }
+}
