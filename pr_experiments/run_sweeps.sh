7656a36832c88a177bd5eef3866e717eb470a201
moreau <moreau@Thierrys-MacBook-Pro.local>
2014-01-06 19:49:21 -0500

    modifications i forgot to commit after the report deadline

--- pr_experiments/run_sweeps.sh
+++ pr_experiments/run_sweeps.sh
@@ -6,6 +6,7 @@ do
     for s in 1 2 4 8 16 32
     do 
         echo "K = $k, merges = $s"
-        eval "../sbt/sbt 'run local input/toy.txt output/top 2 64 ${s} ${k} 1 false'"
+        #eval "../sbt/sbt 'run local input/toy.txt output/top 2 64 ${s} ${k} 1 false'"
+        eval "../sbt/sbt 'run local input/sample.txt output/sample 2 64 ${s} ${k} 100 false'"
     done
 done
